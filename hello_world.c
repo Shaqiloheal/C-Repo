0f99524091a1e5143a61523a6f6035c8c9e951be
Warren Spalding <95550358+Shaqiloheal@users.noreply.github.com>
2022-01-12 22:43:45 +0000

    added hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-int main() {
+#include <stdio.h> // define the header file
+void main() 
+{
     // printf() displays the string inside quotation
     printf("Hello, World!");
-    return 0;
 }
